00ca031fcf90a702155159f3d4edc5c9ad5f0f00
timurista <timothy.urista@gmail.com>
2019-12-13 07:06:14 -0800

    add s3 bucket deployment of json

--- backend-aggregator/lib/backend-aggregator-stack.ts
+++ backend-aggregator/lib/backend-aggregator-stack.ts
@@ -1,16 +1,63 @@
 import cdk = require("@aws-cdk/core");
 import lambda = require("@aws-cdk/aws-lambda");
 import path = require("path");
+import s3deploy = require("@aws-cdk/aws-s3-deployment");
+import s3 = require("@aws-cdk/aws-s3");
+import iam = require("@aws-cdk/aws-iam");
+import cloudfront = require("@aws-cdk/aws-cloudfront");
 
 export class BackendAggregatorStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const fn = new lambda.Function(this, "feedAggregator", {
+    // bucket to handle
+    const bucket = new s3.Bucket(this, "Destination", {
+      versioned: false,
+      bucketName: "tim-urista-web-blog-articles-distribution-v1",
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["s3:*"],
+      resources: [bucket.bucketArn + "/*"]
+    });
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "CFBlogArticlesDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket
+            },
+            behaviors: [{ isDefaultBehavior: true }]
+          }
+        ]
+      }
+    );
+
+    // new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
+    //   sources: [s3deploy.Source.asset("./blogs")],
+    //   destinationBucket: bucket,
+    //   distribution,
+    //   distributionPaths: ["/blogs/*.json"]
+    // });
+
+    const lambdaFn = new lambda.Function(this, "feedAggregator", {
       runtime: lambda.Runtime.NODEJS_10_X,
       handler: "lambda-handler.handler",
-      code: new lambda.AssetCode(path.join(__dirname, "src"))
+      code: new lambda.AssetCode(path.join(__dirname, "src")),
+      environment: {
+        S3_BUCKET: bucket.bucketName,
+        CF_DISTRIBUTION_ID: distribution.distributionId
+      }
     });
+
+    bucket.grantReadWrite(lambdaFn);
+    lambdaFn.addToRolePolicy(s3ReadWritePolicy);
 
     // The code that defines your stack goes here
   }
--- backend-aggregator/lib/src/lambda-handler.ts
+++ backend-aggregator/lib/src/lambda-handler.ts
@@ -1,10 +1,18 @@
-let Parser = require("rss-parser");
-let parser = new Parser();
+const aws = require("aws-sdk");
+// const ManagedUpload = require("@types/aws-sdk").S3.ManagedUpload;
+const Parser = require("rss-parser");
 
 const myMediumArticlesFeed = "https://medium.com/feed/@timothy.urista";
 const aiMediumaArticlesFeed = "https://medium.com/feed/tag/ai";
 const cloudComputingMediumArticlesFeed =
-  "https://medium.com/tag/cloud-computing";
+  "https://medium.com/feed/tag/cloud-computing";
+
+const parser = new Parser();
+const s3Bucket = process.env.S3_BUCKET || "";
+const distributionID = process.env.CF_DISTRIBUTION_ID || "";
+
+const cloudfront = new aws.CloudFront();
+const s3 = new aws.S3();
 
 const allFeeds = [
   myMediumArticlesFeed,
@@ -12,20 +20,52 @@ const allFeeds = [
   cloudComputingMediumArticlesFeed
 ];
 
+type ManagedUpload = {
+  ETag: string;
+};
+
 async function getAllFeeds() {
   let jsonRes: Array<any> = [];
   for (let feedUrl of allFeeds) {
-    console.log(feedUrl);
+    // console.log(feedUrl);
     let feed = await parser.parseURL(feedUrl);
     jsonRes.push(feed);
-    console.log(jsonRes);
+    // console.log(jsonRes);
   }
   return jsonRes;
 }
+
+async function saveFeeds(feeds: any) {
+  const params = {
+    Bucket: s3Bucket,
+    Key: "blogs/articles.json",
+    Body: JSON.stringify(feeds)
+  };
+  var s3UploadPromise = new Promise(function(resolve, reject) {
+    s3.upload(params, function(err: any, data: ManagedUpload) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+  let uploadedFile: any = {};
+  try {
+    uploadedFile = await s3UploadPromise;
+  } catch (e) {
+    throw new Error(e);
+  }
+  console.log("FILE UPLOADED", uploadedFile.ETag);
+  return uploadedFile.ETag;
+}
+
 export const handler = async (event: any = {}): Promise<any> => {
   try {
-    const response = await getAllFeeds();
-    return { statusCode: 200, body: JSON.stringify(response) };
+    const feeds = await getAllFeeds();
+    const feedsBucketUrl = await saveFeeds(feeds);
+    console.log("FEEDS BUCKET", feedsBucketUrl);
+    return { statusCode: 200, body: JSON.stringify(feeds) };
   } catch (err) {
     return { statusCode: 500, body: JSON.stringify(err) };
   }
