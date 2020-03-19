fb90bc26e770f1bc1f0254b1d6ebc442743131f6
timurista <timothy.urista@gmail.com>
2020-03-19 07:08:43 -0700

    add mobile updates to about me page

--- timurista-website/src/pages/About.tsx
+++ timurista-website/src/pages/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Paper, Grid, Typography, Box } from "@material-ui/core";
-import {makeStyles } from '@material-ui/core/styles';
-import Testimonials from "../components/testimonials/Testimonials"
+import { Paper, Grid, Typography, Box, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Testimonials from "../components/testimonials/Testimonials";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,45 +11,78 @@ const useStyles = makeStyles(theme => ({
     fontSize: 16,
     borderRadius: 3,
     marginTop: 60,
-    boxShadow: 'none',
-    minHeight: '400px',
-    padding: theme.spacing(2),
+    boxShadow: "none",
+    minHeight: "400px",
+    padding: theme.spacing(2)
   },
   divider: {
     margin: 20
+  },
+  link: {
+    color: theme.palette.primary.light
   }
 }));
 
 function About() {
-  const classes= useStyles();
-  return <Box>
-    <Grid container>
-      <Paper className={classes.root}>
-      <Typography variant="h3">Proven results</Typography>
+  const classes = useStyles();
+  return (
+    <Box>
+      <Grid container>
+        <Paper className={classes.root}>
+          <Typography variant="h3">Proven results</Typography>
 
-      <Box marginBottom={8} marginTop={8}>
-        <Testimonials />
-      </Box>
+          <Box marginBottom={8} marginTop={8}>
+            <Testimonials />
+          </Box>
 
+          <Typography variant="h3">Proven Track Record</Typography>
+          <Box marginBottom={8} marginTop={2}>
+            <Box display="flex" justifyContent="space-evenly">
+              <Box flex="stretch">
+                <h4>Big names</h4>
+                <ul>
+                  <li>Roku</li>
+                  <li>Facebook</li>
+                </ul>
+              </Box>
 
-      <Typography variant="h3">Solutions for your size</Typography>
-        <Box marginBottom={8} marginTop={8}>
-          * Serverless microsites
-          * SPA blogs
-          * Full end to end Solutions
-          * Devops Support
-        </Box>
+              <Box flex="stretch">
+                <h4>Start ups</h4>
+                <ul>
+                  <li>Aerion</li>
+                  <li>Iterate AI</li>
+                </ul>
+              </Box>
 
-        
-        <Typography variant="h3">Previous Work</Typography>
-        <Box marginBottom={8} marginTop={8}>
-          Ex-Facebook, Software Engineer at Roku.          
-        </Box>
-      
+              <Box flex="stretch">
+                <h4>Health Industry</h4>
+                <ul>
+                  <li>Healthtrio</li>
+                  <li>Weedmaps</li>
+                </ul>
+              </Box>
+            </Box>
 
-    </Paper>
-    </Grid>
-  </Box>;
+          </Box>
+
+          <Typography variant="h3">Mobile and Website Development</Typography>
+          <Box marginBottom={8} marginTop={8}>
+            <img src="/images/timrusitaAppScreenshot.png" height={800} />
+          </Box>
+
+          <Typography variant="h3">Ready to get work done?</Typography>
+          <Box marginBottom={8} marginTop={2}>
+            <Link
+              className={classes.link}
+              href="https://join.slack.com/t/timuristafree-2jp8453/shared_invite/zt-cwsbotut-gEo2hrqF7qGEFt9r9~_SSQ"
+            >
+              Join the Slack channel and get an estimate today!
+            </Link>
+          </Box>
+        </Paper>
+      </Grid>
+    </Box>
+  );
 }
 
 export default About;
