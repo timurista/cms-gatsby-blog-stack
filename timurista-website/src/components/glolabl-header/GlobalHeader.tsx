57a0c882797c5ce4509ae9755ea14c74ae3289f6
timurista <timothy.urista@gmail.com>
2020-07-17 07:10:48 -0700

    add logout and login buttons

--- timurista-website/src/components/glolabl-header/GlobalHeader.tsx
+++ timurista-website/src/components/glolabl-header/GlobalHeader.tsx
@@ -3,28 +3,78 @@ import "./GlobalHeader.scss";
 import HeroImage from "../hero-image/HeroImage";
 import { A } from "hookrouter";
 import { BlogStore } from "../../store/BlogStore";
-import { Button, Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
 import get from "lodash.get";
 import { inject, observer } from "mobx-react";
+import { any } from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5"
+  }
+})((props: any) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center"
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center"
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white
+      }
+    }
+  }
+}))(MenuItem);
 
 export interface GlobalHeaderProps {
   blogStore?: BlogStore;
 }
 
 function GlobalHeader(props: GlobalHeaderProps) {
+  const username = get(props, "authStore.user.username");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <header className="Global-Header">
       <ul className="nav">
-      <HeroImage />
+        <HeroImage />
         <li className="nav-item">
-        
-        <Typography variant="body2">
-          <A className="box" href="/">
-            Tim Urista            
-          </A>
+          <Typography variant="body2">
+            <A className="box" href="/">
+              Tim Urista
+            </A>
           </Typography>
         </li>
-        
+
         <li className="nav-item divider">
           <A href="/blog">Blog</A>
         </li>
@@ -34,10 +84,33 @@ function GlobalHeader(props: GlobalHeaderProps) {
         <li className="nav-item">
           <A href="/blog">Services</A>
         </li>
+        <li className="nav-item">
+          <A href="/privacy">Privacy</A>
+        </li>
 
         <li>
-          {get(props, "authStore.user") ? (
-            <span>{get(props, "authStore.user.username")}</span>
+          {username != null ? (
+            <>
+              <Avatar
+                alt={username}
+                src={`https://robohash.org/${username}`}
+                onClick={handleClick}
+              />
+              <StyledMenu
+                id="customized-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <StyledMenuItem>
+                  <ListItemText
+                    primary="Logout"
+                    onClick={get(props, "authStore.logout")}
+                  />
+                </StyledMenuItem>
+              </StyledMenu>
+            </>
           ) : (
             <Button
               className="login-button"
--- timurista-website/src/store/AuthStore.ts
+++ timurista-website/src/store/AuthStore.ts
@@ -5,8 +5,16 @@ import axios from "axios";
 const initialUrl = window.location.href;
 export const CONGITO_ENDPOINT = `https://thetimurista.auth.us-east-1.amazoncognito.com`;
 export const COGNITO_AUTH_LOGIN = `https://thetimurista.auth.us-east-1.amazoncognito.com/login?client_id=2k9cu38mg2h3ugqnpvc51gbg3a&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=${window.location.href}`;
+// export const COGNITO_AUTH_LOGOUT = `https://thetimurista.auth.us-east-1.amazoncognito.com/logout?client_id=2k9cu38mg2h3ugqnpvc51gbg3a&response_type=token&redirect_uri=${window.location.href}logout`;
+export const COGNITO_AUTH_LOGOUT = `https://thetimurista.auth.us-east-1.amazoncognito.com/logout?client_id=2k9cu38mg2h3ugqnpvc51gbg3a&logout_uri=${encodeURIComponent(
+  window.location.href
+)}`;
+
+const TOKEN_LS_KEY = "timurista.com-temp-token";
+
 // export const TOKEN_VERIFIER_URL = `${CONGITO_ENDPOINT}/oauth2/userInfo&scope=openid`;
-export const TOKEN_VERIFIER_URL = 'https://g2vtb5opa8.execute-api.us-east-1.amazonaws.com/Prod/userInfo'
+export const TOKEN_VERIFIER_URL =
+  "https://g2vtb5opa8.execute-api.us-east-1.amazonaws.com/Prod/userInfo";
 
 export interface User {
   username: string;
@@ -22,52 +30,47 @@ function parseToken() {
 }
 
 function setWithExpiry(key: string, value: string, ttl: number) {
-  const now = new Date()
-  
+  const now = new Date();
+
   // `item` is an object which contains the original value
   // as well as the time when it's supposed to expire
-	const item = {
-		value: value,
-		expiry: now.getTime() + ttl
-	}
-	localStorage.setItem(key, JSON.stringify(item))
+  const item = {
+    value: value,
+    expiry: now.getTime() + ttl
+  };
+  localStorage.setItem(key, JSON.stringify(item));
 }
 
 function getWithExpiry(key: string) {
-	const itemStr = localStorage.getItem(key)
-	// if the item doesn't exist, return null
-	if (!itemStr) {
-		return null
-	}
-	const item = JSON.parse(itemStr)
-	const now = new Date()
-	// compare the expiry time of the item with the current time
-	if (now.getTime() > item.expiry) {
-		// If the item is expired, delete the item from storage
-		// and return null
-		localStorage.removeItem(key)
-		return null
-	}
-	return item.value
+  const itemStr = localStorage.getItem(key);
+  // if the item doesn't exist, return null
+  if (!itemStr) {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  // compare the expiry time of the item with the current time
+  if (now.getTime() > item.expiry) {
+    // If the item is expired, delete the item from storage
+    // and return null
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
 }
 
 export class AuthStore {
   @observable user: User | null = null;
 
   constructor() {
-    const TOKEN_LS_KEY = 'timurista.com-temp-token';
-    let token = getWithExpiry(TOKEN_LS_KEY)
-    let fetchFromUrl = false
-    if (!token) {      
+    let token = getWithExpiry(TOKEN_LS_KEY);
+    let fetchFromUrl = false;
+    if (!token) {
       token = parseToken();
-      // window.location.replace(initialUrl);
     }
-    // if (token && fetchFromUrl) {
-    //   window.location.replace(initialUrl);
-    // }
-    
+
     this.fetchUserDetails(token);
-    setWithExpiry(TOKEN_LS_KEY, token, 12*60*60*1000)
+    setWithExpiry(TOKEN_LS_KEY, token, 12 * 60 * 60 * 1000);
   }
 
   @action async fetchUserDetails(token: string) {
@@ -76,23 +79,29 @@ export class AuthStore {
       url: TOKEN_VERIFIER_URL,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Authorization": token
+        Authorization: token
       }
     }).then(res => {
       console.log("success", res);
       runInAction(() => {
         this.user = {
           username: res.data.Username
-        }
+        };
         if (res.data.UserAttributes) {
-          res.data.UserAttributes.forEach((att: { Name: string, Value: string }) => {
-            // this.user[att.Name] = att.Value
-          })
+          res.data.UserAttributes.forEach(
+            (att: { Name: string; Value: string }) => {
+              // this.user[att.Name] = att.Value
+            }
+          );
         }
-
-      })
-
+      });
     });
+  }
+
+  @action
+  logout() {
+    window.location.assign(COGNITO_AUTH_LOGOUT);
+    localStorage.removeItem(TOKEN_LS_KEY);
   }
 
   @action
