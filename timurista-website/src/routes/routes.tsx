0a3cca83212a9a8ed0a1c7e4aa843fb169bd3d14
timurista <timothy.urista@gmail.com>
2020-07-16 17:26:24 -0700

    add privacy policy page

--- timurista-website/src/routes/routes.tsx
+++ timurista-website/src/routes/routes.tsx
@@ -4,10 +4,12 @@ import AboutPage from "../pages/About";
 import BlogDetail from "../pages/BlogDetail";
 import PaperDetail from "../pages/PaperDetail";
 import ProductOverview from "../pages/Home";
+import PrivacyPage from "../pages/PrivacyPage";
 
 const routes = {
   "/": () => <HomePage />,
   "/about": () => <AboutPage />,
+  "/privacy": () => <PrivacyPage />,
   "/blog": () => <ProductOverview />,
   "/blog/:id": ({ id }: any) => <BlogDetail id={id} />,
   "/paper/:id": ({ id }: any) => <PaperDetail id={id} />
